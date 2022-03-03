e37fa30ca476f36e567d9551cf1725cba93e3f4b
Sahak-S <sahak.sa@mail.ru>
2022-03-03 23:48:57 +0400

    add

---
+++ src/main/java/com/example/springmyitems/cantroller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.springmyitems.cantroller;
+
+import com.example.springmyitems.entity.User;
+import com.example.springmyitems.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/addUser")
+    public String addUserPage(){
+        return "saveUser";
+
+    }
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user){
+        userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editUser/{id}")
+    public String editUserPage(ModelMap map,@PathVariable("id") int id){
+        Optional<User> userById = userRepository.findById(id);
+        if (userById.isPresent()){
+            map.addAttribute("user",userById.get());
+            return "saveUser";
+        }else {
+            return "redirect:/";
+        }
+
+
+    }
+}
